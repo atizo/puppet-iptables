f421e1fcfb200c0d00929dedb785e70c38ce3873
Marcel Haerry <haerry@puzzle.ch>
2008-04-29 18:42:21 +0200

    added iptables disabler

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,4 +6,22 @@
 
 
 # modules_dir { "iptables": }
-class iptables {}
+class iptables {
+    include iptables::base 
+}
+
+class iptables::base {
+    package{[ 'iptables', 'iptables-ipv6']:
+        ensure => present,
+    }
+    service{iptables:
+        ensure => running,
+        enable => true,
+        require => Package[iptables],
+    }
+    service{ip6tables:
+        ensure => running,
+        enable => true,
+        require => Package[iptables-ipv6],
+    }
+}
