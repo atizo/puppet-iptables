69d9a14ee7c2e804544316214ee5f4cb4f1214a6
Marcel Haerry <haerry@puzzle.ch>
2008-04-29 18:38:33 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# iptables module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "iptables": }
+class iptables {}
