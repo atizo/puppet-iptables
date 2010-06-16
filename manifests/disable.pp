1d152b58e3b03edd82918fcedf463463fd570984
Simon Josi <josi@puzzle.ch>
2010-06-16 15:22:55 +0200

    enable hasstatus for service

--- manifests/disable.pp
+++ manifests/disable.pp
@@ -9,9 +9,11 @@ class iptables::disable inherits iptables::base {
     Service[iptables]{
         ensure => stopped,
         enable => false,
+        hasstatus => true,
     }
     Service[ip6tables]{
         ensure => stopped,
         enable => false,
+        hasstatus => true,
     }
 }
