f6249888d8550b8016b9bbb52b2729cd0ee2def9
Simon Josi <simon.josi@atizo.com>
2010-11-12 19:52:25 +0100

    add new copyright notice; switch to two space indentation

--- manifests/disable.pp
+++ manifests/disable.pp
@@ -1,12 +1,12 @@
-class iptables::disable inherits iptables::base {
-    Service['iptables'] {
-        ensure => stopped,
-        enable => false,
-        hasstatus => true,
-    }
-    Service['ip6tables'] {
-        ensure => stopped,
-        enable => false,
-        hasstatus => true,
-    }
+class iptables::disable inherits iptables {
+  Service['iptables'] {
+    ensure => stopped,
+    enable => false,
+    hasstatus => true,
+  }
+  Service['ip6tables'] {
+    ensure => stopped,
+    enable => false,
+    hasstatus => true,
+  }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,10 @@
 # iptables module
 #
 # Copyright 2008, Puzzle ITC GmbH
+# Copyright 2010, Atizo AG
 # Marcel Härry haerry+puppet(at)puzzle.ch
-# Simon Josi josi+puppet(at)puzzle.ch
+# Simon Josi simon.josi+puppet(at)atizo.com
+
 #
 # This program is free software; you can redistribute 
 # it and/or modify it under the terms of the GNU 
@@ -12,21 +14,17 @@
 #
 
 class iptables {
-    include iptables::base 
-}
-
-class iptables::base {
-    package{ [ 'iptables', 'iptables-ipv6']:
-        ensure => present,
-    }
-    service{'iptables':
-        ensure => running,
-        enable => true,
-        require => Package['iptables'],
-    }
-    service{'ip6tables':
-        ensure => running,
-        enable => true,
-        require => Package['iptables-ipv6'],
-    }
+  package{ ['iptables', 'iptables-ipv6']:
+    ensure => present,
+  }
+  service{'iptables':
+    ensure => running,
+    enable => true,
+    require => Package['iptables'],
+  }
+  service{'ip6tables':
+    ensure => running,
+    enable => true,
+    require => Package['iptables-ipv6'],
+  }
 }
